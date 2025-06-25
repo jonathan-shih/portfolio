d26bc54688ff6d2334fd0702f377bd394bae9dfc
jonathan-shih <jonathan25shih@gmail.com>
2025-06-25 13:18:04 -0700

    fix test and add wip workflow block

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App.js';
 
+beforeEach(() => {
+  // Mock IntersectionObserver
+  global.IntersectionObserver = class IntersectionObserver {
+    constructor() {}
+
+    observe() {
+      return null;
+    }
+
+    unobserve() {
+      return null;
+    }
+
+    disconnect() {
+      return null;
+    }
+  };
+});
+
 test('renders correctly', () => {
   render(<App />);
   const linkElement = screen.getAllByText(/Jonathan Shih/i);
